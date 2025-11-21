dcae01da4b87ca05ecb97bf2572cfb49382dc8dd
Ed <elh-dev@proton.me>
2025-11-21 11:03:56 +1100

    Added storeCredLoop function and Erase .txt option

--- PManager.py
+++ PManager.py
@@ -3,7 +3,7 @@
 rotValue = 3
 # Debug option 1 for on 
 debug = 0
-def debugOption(x,debug):
+def debugOption(x, debug):
     if debug == 1:
         print(x)
 
@@ -22,39 +22,47 @@ def listToString(l):
 # Functions for .txt manipulation
 
 # Adds string to text document
-def writeToText(y,debug):
+def writeToText(y, debug):
     with open('text.txt', 'a') as file:
-        file.write(y + '\n')
-
-    #debugOption("Text appended",debug)
+         file.write(y 
+                    + '\n')
+    # Print veriable if debug is on
+    debugOption("Text appended", debug)
 # Overwrites string to text document 
 def overwriteToText(y,debug):
     with open('text.txt', 'w') as file:
-        file.write(y + '\n') 
-
-    debugOption("Text appended",debug)
+         file.write(y 
+                    + '\n') 
+    # Print veriable if debug is on
+    debugOption("Text appended", debug)
+# Erase text document content 
+def eraseText():
+    with open('text.txt', 'w') as file:
+         pass
 # Read text to memory 
 def textToMemory(debug):
     with open('text.txt', 'r') as file:
-        line = file.readlines()
-    debugOption(line,debug)
+         line = file.readlines()
+    # Print veriable if debug is on
+    debugOption(line, debug)
+
     return line
 # Un-used feature for future implimentation
-def textEditer(x,text,debug):
+def textEditer(x, text, debug):
     with open('text.txt', 'r') as file:
-        line = file.readlines()
+         line = file.readlines()
+    # Print veriable if debug is on
     debugOption(line,debug)
+
     line[x-1] = text + '\n'
     with open('text.txt', 'w') as file:
-        file.writelines(line) 
+         file.writelines(line) 
 
 # Functions for Security
  
 # Encode and decode using y as the string and t as the veraible to decide 1 = encrypt or else = decrypt 
-def cipher(y,t):
-
-    
-    #ecrypty
+def cipher(y, t, debug):
+    #ecrypt
     if t == 1:
         rotValuePresent = rotValue
     #decrypt
@@ -63,8 +71,8 @@ def cipher(y,t):
     #Assigns characters to list 
     listCipher = stringToList(y)
     
-    #prints list
-    debugOption(listCipher,debug)
+    # Print veriable if debug is on
+    debugOption(listCipher, debug)
 
     #Retrives length of list assigned to x 
     listLen = len(listCipher)
@@ -72,40 +80,39 @@ def cipher(y,t):
     for i in range(listLen):
         #transaltes charcater to number and Assigns to veriable 
         numberValue = ord(listCipher[i])
-
-        debugOption(numberValue,debug)
+        # Print veriable if debug is on
+        debugOption(numberValue, debug)
         #Assigns rot3 encoded value to veriable 
         numberValueNew = numberValue + rotValuePresent
-
-        debugOption(numberValueNew,debug)
+        # Print veriable if debug is on
+        debugOption(numberValueNew, debug)
         #Translates encoded number to character and Assigns to veriable 
         stringValue = chr(numberValueNew)
         listCipher[i] = stringValue
-    #prints encoded list 
-    debugOption(listCipher,debug)
+    # Print veriable if debug is on
+    debugOption(listCipher, debug)
     string = listToString(listCipher)
     return string
-# 
-def stringToRot(string,debug):
-    if debug == 1:
-        choice = 1
-    else:
-        print("Enter 1 to encrypt else decrypt")
-        choice = input()
+
+# Proglem here with debug +++++++++++++++++++++++++++++++++++ 
+def stringToRot(string, debug):
     # Assigns encrypted string to veriable 
-    y = cipher(string,int(choice))
+    y = cipher(string, 1, 0)# fix debug
     # Adds encrypted string to .txt
-    writeToText(y,debug)
-    
-    #debugOption(y,debug)
+    writeToText(y, 0)
+    # Print veriable if debug is on
+    debugOption(y, 0)# fix debug
 # Deciphers entire text document in to memory 
 def textDecipher(debug):
     lines = textToMemory(debug)
     listLen = len(lines)
     for i in range(listLen):
-        lines[i] = cipher(lines[i],0)
-        debugOption(lines[i],debug)
-    debugOption(lines,debug)
+        lines[i] = cipher(lines[i], 0, debug)
+        # Print veriable if debug is on
+        debugOption(lines[i], debug)
+    # Print veriable if debug is on
+    debugOption(lines, debug)
+
     return lines 
 # Prints name and password with indicators
 def printText(text):
@@ -129,52 +136,36 @@ def printText(text):
 
 # Funtions fore Option Menu
 
+#
+def storeCred(x,confirm, debug):
+    print(x)
+    password = input()
+    print("-----------------------")
+    print("Enter 1 to Confirm")
+    print("Enter 0 to Retry")
+    print("-----------------------")
+    option = input()
+    print("-----------------------")
+
+    if option == "1":
+        stringToRot(password, debug)
+        confirm = 1
+    return confirm
+
+
 # Store Credentials
-def storeCred():
+def storeCredLoop(debug):
     confirm = 0
     while confirm == 0:
-        print("Enter UserName")
-        password = input()
-        print("-----------------------")
-        print("Enter 1 to Confirm")
-        print("Enter 0 to Retry")
-        print("-----------------------")
-        option = input()
-        print("-----------------------")
-        if option == "1":
-            
-            stringToRot(password, 1)
-            confirm = 1
+        confirm = storeCred("Enter Username", confirm, debug)
     
-    confirm2 = 0
-    while confirm2 == 0:
-        
-        print("Enter Password") 
-        key = input()
-        print("-----------------------")
-        print("  Enter 1 to Confirm")
-        print("  Enter 0 to Retry")
-        print("-----------------------")
-        option2 = input()
-        print("-----------------------")
-        if option2 == "1":
-            stringToRot(key, 1)
-            confirm2 = 1 
+    confirm = 0
+    while confirm == 0:
+        confirm = storeCred("Enter Password", confirm, debug)
 
-    confirm3 = 0
-    while confirm3 == 0:
-        
-        print("Enter URL/resource") 
-        url = input()
-        print("-----------------------")
-        print("  Enter 1 to Confirm")
-        print("  Enter 0 to Retry")
-        print("-----------------------")
-        option3 = input()
-        print("-----------------------")
-        if option3 == "1":
-            stringToRot(url, 1)
-            confirm3 = 1 
+    confirm = 0
+    while confirm == 0:
+        confirm = storeCred("Enter URL", confirm, debug)
 
 # View Credentials
 def viewCred(debug):
@@ -199,8 +190,8 @@ def edit(debug):
     print("-----------------------")
     newtext = input()
     print("-----------------------")
-    cipheredText = cipher(newtext,1)
-    textEditer(lineInt,cipheredText,debug)
+    cipheredText = cipher(newtext, 1,debug )
+    textEditer(lineInt, cipheredText, debug)
 
 def secretMenu(debug):
     print("_______________________")
@@ -208,7 +199,8 @@ def secretMenu(debug):
     print("     Secret Menu")
     print("_______________________")
     print("-----------------------")
-    print("1: edit Log In Details")
+    print("1: Edit Log In Details")
+    print("2: Erase Text Document")
     print("0:        Exit")
     print("-----------------------")
     secretMenu = input()
@@ -216,11 +208,13 @@ def secretMenu(debug):
     match secretMenu:
         case "1":
             edit(debug)
+        case "2":
+            eraseText()
         case _:
             print("Invalid Option")
 
 # Option Menu
-def optionMenu(exit,debug):
+def optionMenu(exit, debug):
     print("_______________________")
     print()
     print(" ELH Password Manager")
@@ -234,7 +228,7 @@ def optionMenu(exit,debug):
     print("-----------------------")
     match optionMenu:
         case "1":
-            storeCred()
+            storeCredLoop(debug)
         case "2":
             viewCred(debug)
         case "0":
@@ -251,10 +245,6 @@ def optionMenu(exit,debug):
 # Main
 exit = False
 while exit != True:
-    exit = optionMenu(exit,debug)
-
-
-
-
+    exit = optionMenu(exit, debug)
 
 
